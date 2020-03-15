1fcff72f790f2bafb8556b25097b200027d12b75
emma levi <emmaa-l@hotmail.fr>
2020-03-15 21:09:54 +0200

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+  def ask
+    @question = params[:question]
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = coach_answer
+  end
+
+  def coach_answer
+    if @question == 'I am going to work'
+      "Great!"
+    elsif @question.include? "?"
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+end
